7ca86a6b748bc7ea94ad77ad9935c63f265b6732
Agatha28 <agasantoso7@gmail.com>
2020-11-10 08:59:17 +0700

    secondcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useCallback} from "react";
+import {Hello} from "./Hello";
+import {Square} from "./Square";
 
-function App() {
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const favoriteNums = [7, 21, 37]
+
+  const increment = useCallback((n) => {
+    setCount(c => c + n);
+  }, 
+  [setCount]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello increment = {increment}/>
+      <div>count: {count}</div>
+      {favoriteNums.map(n => {
+        return(
+          <Square increment={increment} n={n} key={n}/>
+        )
+      })}
     </div>
   );
-}
+};
 
 export default App;
